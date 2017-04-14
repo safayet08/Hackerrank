405b4ae4b39ab328b8fdd11587ab87c2b5d97d73
Safayet Islam Anonn'o <anonno.anonno@gmail.com>
2017-04-14 17:02:39 +0600

    Update Begin-End.cpp

--- Begin-End.cpp
+++ Begin-End.cpp
@@ -27,29 +27,3 @@ int main()
     printf("%lld\n",ans);
     return 0;
 }
-#include<bits/stdc++.h>
-using namespace std;
-
-int letters[26];
-int cum[26];
-
-int main()
-{
-    char a;
-    int sz;
-    scanf("%d",&sz);
-    int temp;
-    while(sz)
-    {
-        cin >> a;
-        letters[a - 97] ++;
-        temp = letters[a - 97];
-        cum[a - 97] += temp;
-        sz--;
-    }
-    long long ans = 0;
-    for(int i=0; i<26; i++)
-        ans += cum[i];
-    printf("%lld\n",ans);
-    return 0;
-}
